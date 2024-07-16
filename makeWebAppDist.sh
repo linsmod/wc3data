37af84efa43301a8b4be0134fec42e71a1e1a91d
wahahahaha <tiny@sandsea.info>
2024-07-16 09:46:03 +0800

    allows render TOC and FDF files as TEXT

--- makeWebAppDist.sh
+++ makeWebAppDist.sh
@@ -1,4 +1,15 @@
 export NODE_OPTIONS=--openssl-legacy-provider
-npm run build && [ -d "/work/DENV/AndroidStudioProjects/wc3dataAnServ/app/src/main/assets/www/" ] &&
-    rm -rf /work/DENV/AndroidStudioProjects/wc3dataAnServ/app/src/main/assets/www/ &&
-    cp -R /wp4/war3dev/wc3data/build /work/DENV/AndroidStudioProjects/wc3dataAnServ/app/src/main/assets/www
+copy_from=/wp4/war3dev/wc3data/build
+android_assets=/work/DENV/AndroidStudioProjects/wc3dataAnServ/app/src/main/assets/www
+javaapp_assets=/sn750/data-wc3hostAssets/www
+
+forceCopy() {
+    rm -rf "$android_assets"
+    cp -R "$copy_from" "$android_assets"
+
+    rm -rf "$javaapp_assets"
+    cp -R "$copy_from" "$javaapp_assets"
+}
+
+npm run build && forceCopy
+    
