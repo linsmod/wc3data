966a7c804d4b807f63c7c60f7e6992f9551de6f9
wahahahaha <tiny@sandsea.info>
2024-08-04 11:07:17 +0800

    use local wc3Data

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(proxy('/api', {
-    target: 'http://192.168.1.106:5200/',
+    target: 'http://127.0.0.1:5200/',
     changeOrigin: true,
     headers: {
       "Connection": "keep-alive",
